090e16ad6509e361e4d584d43ec9efe345e0662b
Dmitriq Berlin <dmtrbrln@gmail.com>
2019-10-06 15:45:29 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = check
+function check(str, bracketsConfig) {
+    function get_index(letter){
+        for (let index = 0; index < bracketsConfig.length; index++) {
+            if(bracketsConfig[index].indexOf(letter) != -1) {
+                if(bracketsConfig[index][0] != bracketsConfig[index][1]){
+                    return [index, bracketsConfig[index].indexOf(letter)];
+                }
+                else{
+                    return [index, -1];
+                }
+            }
+        }
+    }
+    let conf = new Array();
+    for (let i = 0; i < str.length; i++) {
+        conf.push(get_index(str[i]));
+        if (conf.length >= 2 && conf[conf.length-1][0] == conf[conf.length-2][0] && (conf[conf.length-1][1] > conf[conf.length-2][1] || conf[conf.length-1][1] == -1)) {
+            conf.pop();
+            conf.pop();
+        }
+    }
+    if (conf.length == 0) { 
+        return true;
+    }
+    else {
+        return false;
+    }
 }
